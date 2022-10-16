51b67f7834a2f19443c9c0ef30673232b53f74de
Thomas Willson <thomaswilllson@yahoo.com>
2022-10-16 13:16:34 -0700

    Added POST and GET feature for medication list

--- routes/medications.js
+++ routes/medications.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const postsController = require("../controllers/posts");
+const medicationsController = require("../controllers/medications");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Post Routes - simplified for now
-router.get("/:id", ensureAuth, postsController.getPost);
+//Med Routes - simplified for now
+router.get("/:id", ensureAuth, medicationsController.getMed);
 
-router.post("/createPost", upload.single("file"), postsController.createPost);
+router.post("/createMed", upload.single("file"), medicationsController.createMed);
 
-router.put("/likePost/:id", postsController.likePost);
+// router.put("/likePost/:id", postsController.likePost);
 
-router.delete("/deletePost/:id", postsController.deletePost);
+// router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
