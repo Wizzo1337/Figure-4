f412af7f81e6c8259ec6ffdfdf902c18fe98bfe0
Thomas Willson <thomaswilllson@yahoo.com>
2022-10-30 21:10:32 -0700

    Added a public button to make posts public and made feed only for public posts

--- routes/posts.js
+++ routes/posts.js
@@ -11,6 +11,8 @@ router.post("/createPost", upload.single("file"), postsController.createPost);
 
 router.put("/likePost/:id", postsController.likePost);
 
+router.put("/sharePost/:id", postsController.sharePost)
+
 router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
