c1be103951445e6e128e42f63ce898d69454e3a9
Thomas Willson <thomaswilllson@yahoo.com>
2022-12-02 01:55:15 -0800

    Added User Names for each event added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const cloudinary = require('../middleware/cloudinary');
 const Post = require('../models/Post');
 const Comment = require('../models/Comment');
+const User = require('../models/User');
 
 module.exports = {
 	getProfile : async (req, res) => {
@@ -22,9 +23,9 @@ module.exports = {
 	getPost    : async (req, res) => {
 		try {
 			const post = await Post.findById(req.params.id);
-			console.log(post);
+			const posts = await User.find({_id: post.user});
 			const comments = await Comment.find({post: req.params.id}).sort({createdAt: 'desc'}).lean();
-			res.render('post.ejs', {post: post, user: req.user, comments: comments});
+			res.render('post.ejs', {post: post, posts: posts, user: req.user, comments: comments});
 		} catch (err) {
 			console.log(err);
 		}
